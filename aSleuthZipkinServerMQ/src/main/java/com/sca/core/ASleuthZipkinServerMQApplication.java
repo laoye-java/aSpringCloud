30098e6850e86bf84d2655e3e3897ff9717765f8
laoye-java <82153402@qq.com>
2018-10-17 18:54:02 +0800

    sleuth-zipkin服务追踪 + MQ实现
    Signed-off-by: laoye-java <82153402@qq.com>

--- aSleuthZipkinServerMQ/src/main/java/com/sca/core/ASleuthZipkinServerMQApplication.java
+++ aSleuthZipkinServerMQ/src/main/java/com/sca/core/ASleuthZipkinServerMQApplication.java
@@ -3,7 +3,7 @@ package com.sca.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
-import zipkin.server.EnableZipkinServer;
+//import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 //@EnableZipkinServer  //改为使用Stream方式启动ZipkinServer
