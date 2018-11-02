930efd9c7dbc3bf03231aa7c1dcadac372397257
laoye-java <82153402@qq.com>
2018-11-02 18:04:45 +0800

    增加maven打包插件
    Signed-off-by: laoye-java <82153402@qq.com>

---
+++ aConfigServer/src/main/resources/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash  
+
+main_jar_name=aConfigServer.jar
+
+PID=`jps -l | grep $main_jar_name | awk '{print $1}'`
+
+if [ $PID ]; then
+	kill -9 $PID
+fi
+
+nohup java -jar $main_jar_name -server -Xmx150m -Xms150m -Xmn100m -XX:-PrintGC -XX:-PrintGCDetails -XX:-PrintGCTimeStamps -Xloggc:logs/gc.log &
+
+PID=0
+PID=`jps -l | grep $main_jar_name | awk '{print $1}'`
+
+if [ $PID -ne 0 ]; then
+  echo "(pid=$PID) [Start OK  $PID ]"
+else
+  echo "[Start Failed]"
+fi
+
+tailf nohup.out
---
+++ bClientService/src/main/resources/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash  
+main_jar_name=bClientService.jar
+PID=$(ps -ef | grep $main_jar_name | grep -v grep | awk '{ print $2 }')  
+if [ -z "$PID" ]  
+then  
+    echo $main_jar_name is already stopped  
+else  
+    echo $main_jar_name stop successs!
+    kill -9 $PID  
+fi
