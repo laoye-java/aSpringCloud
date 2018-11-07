e6e9bd89ca98aa31a96fc28c8bb416b4b401d50e
laoye-java <82153402@qq.com>
2018-11-07 14:29:03 +0800

    rabbitmq-bus-client

---
+++ bClientServiceRabbitMQ/src/main/java/com/sca/core/BClientRabbitMQApplication.java
@@ -0,0 +1,16 @@
+package com.sca.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
+public class BClientRabbitMQApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BClientRabbitMQApplication.class,args);
+    }
+}
+
